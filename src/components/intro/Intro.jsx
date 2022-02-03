1deea93abad3e0ae17e549fc434b5403bcbfca05
GraeDev <petergraeson@yahoo.com>
2022-02-04 01:02:33 +1100

    adding content to intro page
    setting up the layout of the intro page using an image from our asset class

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/portfolio_picture.png" alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
             
         </div>
     )
