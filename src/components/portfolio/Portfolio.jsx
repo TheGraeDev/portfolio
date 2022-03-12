cbac68778ac79cd9a7dfdc35bca0f3bd20eb50ef
TheGraeDev <95461233+TheGraeDev@users.noreply.github.com>
2022-03-13 09:16:16 +1100

    Update for portfolio page
    Just added a few example projects into the page. so it can be used as bases to add projects to in the future

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Unity Project</li>
+                <li>Mobile App</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://p7.hiclipart.com/preview/524/532/32/unity-video-games-minecraft-handheld-devices-video-game-development-minecraft.jpg" alt="" />
+                    <h3>Unity Project</h3>
+                </div>
+            </div>
         </div>
     )
 }
